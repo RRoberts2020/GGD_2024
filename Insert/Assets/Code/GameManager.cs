7cd62abdc3a7ca3c95dab2f8d2b6f8b237b54612
Reece Roberts <Reece2.Roberts@live.uwe.ac.uk>
2024-01-27 19:22:19 +0000

    Gameplay loop functional
    Needs bug fixes

---
+++ Insert/Assets/Code/GameManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject Stage1;
+
+    public GameObject Stage2;
+
+    public GameObject Stage3;
+
+    public bool isStage2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Stage1.SetActive(true);
+        Stage2.SetActive(false);
+        Stage3.SetActive(false);
+
+        isStage2 = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Reset()
+    {
+        Stage1.SetActive(true);
+        Stage2.SetActive(false);
+        Stage3.SetActive(false);
+
+        isStage2 = false;
+
+        // Reset vaules
+
+        //Score, timer, words
+    }
+
+    public void Stage2UI() 
+    {
+        Stage1.SetActive(false);
+        Stage2.SetActive(true);
+        Stage3.SetActive(false);
+
+        isStage2 = true;
+    }
+
+    public void Stage3UI()
+    {
+        Stage1.SetActive(false);
+        Stage2.SetActive(false);
+        Stage3.SetActive(true);
+
+        isStage2 = false;
+    }
+}
---
+++ Insert/Assets/Code/JokeScore.cs
@@ -0,0 +1,121 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class JokeScore : MonoBehaviour
+{
+    //Stage 1 stuff?
+
+
+    //Stage 2 stuff
+    public int bonusScore;
+
+    private int noLike;
+
+    private int ableToVote;
+
+    private float timeToReturn = 15f;
+
+    public TextMeshProUGUI countdownText;
+
+    public TextMeshProUGUI yesText;
+
+    public TextMeshProUGUI noText;
+
+    //Stage 3 stuff
+
+    public GameManager Scene2;
+
+    public GameManager Scene3;
+
+    public TextMeshProUGUI playerResultText;
+
+    public TextMeshProUGUI AiResultText;
+
+    public TextMeshProUGUI EndResultText;
+
+    public int EndResultVaule;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bonusScore = 0;
+        noLike = 0;
+        ableToVote = 5;
+    }
+
+    private void Update()
+    {
+        if (Scene2.isStage2 == true)
+        {
+            if (timeToReturn > 0)
+            {
+                timeToReturn -= Time.deltaTime;
+                DisplayTime(timeToReturn);
+            }
+
+            if (timeToReturn <= 0 || ableToVote <= 0)
+            {
+                countdownText.text = string.Format("Times up, no more votes");
+                ableToVote = -1;
+
+                StartCoroutine(EndOfRound());
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        // Convert the time to seconds and milliseconds
+        int seconds = Mathf.FloorToInt(timeToDisplay);
+        int milliseconds = Mathf.FloorToInt((timeToDisplay - seconds) * 100);
+
+        // Update the UI text element with the formatted time
+        countdownText.text = string.Format(" Vote now! - {0:00}:{1:00}", seconds, milliseconds);
+    }
+
+    public void YesJoke()
+    {
+        if (ableToVote >0) 
+        {
+            bonusScore++;
+            ableToVote--;
+
+            yesText.text = "Yes: " + bonusScore.ToString();
+        }
+    }
+
+    public void NoJoke()
+    {
+        if (ableToVote >0)
+        {
+            noLike++;
+            ableToVote--;
+
+            noText.text = "No: " + noLike.ToString();
+        }
+    }
+
+    IEnumerator EndOfRound()
+    { 
+        yield return new WaitForSeconds(3);
+
+        Scene2.Stage2.SetActive(false);
+        Scene3.Stage3.SetActive(true);
+
+        playerResultText.text = "Number of yes votes: " + bonusScore.ToString() + "Number of no votes: " + noLike.ToString(); ;
+       
+        //AiResultText.text = "Audience rating: " + noLike.ToString();
+
+        EndResultVaule = (bonusScore /*+ Audiance score */ );
+
+        EndResultText.text = "Total rating: " + EndResultVaule.ToString();
+
+        StopCoroutine(EndOfRound());
+    }
+
+
+}
